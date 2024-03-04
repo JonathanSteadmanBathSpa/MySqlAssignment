a75fd90d1cf5203d828728ab362b64ac923ced42
JonathanSteadmanBathSpa <146035870+JonathanSteadmanBathSpa@users.noreply.github.com>
2024-03-04 19:59:38 +0000

    This has all data for booking and the history people and the leactures.
    Also linked the tables together.
    Improvement? Need to work on the comments and doing small task for the documentation. :)

--- Historical-Figures.sql
+++ Historical-Figures.sql
@@ -1,7 +1,7 @@
 -- Just checking for the database that is in my system Testing to see if I got access. 
 show databases;
 -- Lets create a new database.
-
+ 
 Create database if not exists Historical_Figures;
 show databases;
 
