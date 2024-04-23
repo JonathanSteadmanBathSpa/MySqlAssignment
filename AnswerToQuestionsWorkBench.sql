9e2b87d8873a1aa7a78748ab3e7f15ef215731e0
JonathanSteadmanBathSpa <146035870+JonathanSteadmanBathSpa@users.noreply.github.com>
2024-04-23 14:14:31 +0100

    Small message to the marker

--- AnswerToQuestionsWorkBench.sql
+++ AnswerToQuestionsWorkBench.sql
@@ -1,3 +1,4 @@
+-- This was used to answer questions calcualtions  and should not be marked. There is a word document on the answers and questions. 
 use Historical_Figures;
 SET SQL_SAFE_UPDATES = 0;
 show tables;
@@ -19,7 +20,7 @@ where BookingsID = 2;
 select * from tbl_Bookings;
 
 /*Adding in a new sciencetist:*/
-Insert into tbl_Historical_People(Name,BirthYear,DeathYear,MainSector,Country_Of_Birth,Description, BookingsID)
+Insert into tbl_Historical_People(Name,BirthYear,DeathYear,MainSector,Country_Of_Birth,Description)
 values("Askar Dzhumadildayev",1956,NULL,"Physics, mathematics","Kazakhstan","
  Dzhumadildayev is a Kazakh mathematician, doctor of physics and mathematics, 
  professor, Full Member of the Kazakhstan National Academy of Science. He was also 
@@ -29,7 +30,7 @@ in the Science category of the national project El Tulgasy (Name of the Motherla
 The idea of the project was to select the most significant citizens of Kazakhstan whose
  names are now associated with the achievements of the country. More than 350,000 people
  voted in this project, and Dzhumadilyavev was voted into second place in his category. 
-",null);
+");
 select * from tbl_Historical_People;
 
 
