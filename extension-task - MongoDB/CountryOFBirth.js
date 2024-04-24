9ddb61801cb93de23b0edc0a29667dd679f2ef84
JonathanSteadmanBathSpa <146035870+JonathanSteadmanBathSpa@users.noreply.github.com>
2024-04-24 15:18:17 +0100

    Add files via upload

---
+++ extension-task - MongoDB/CountryOFBirth.js
@@ -0,0 +1,20 @@
+db.createCollection("CountryOfBirth")
+db.CountryOfBirth.insertMany(
+    [
+        {CountryID: 1, CountryName: "Russia"},
+        {CountryID: 2, CountryName: "Britain"},
+        {CountryID: 3, CountryName: "Iran"},
+        {CountryID: 4, CountryName: "France"},
+        {CountryID: 5, CountryName: "USA"},
+        {CountryID: 6, CountryName: "Egypt"},
+        {CountryID: 7, CountryName: "Germany"},
+        {CountryID: 8, CountryName: "Italy"},
+        {CountryID: 9, CountryName: "Japan"},
+        {CountryID: 10, CountryName: "Poland"},
+        {CountryID: 12, CountryName: "Sweden"},
+        {CountryID: 13, CountryName: "Serbia"},
+        {CountryID: 14, CountryName: "Kazakhstan"},
+
+
+    ]
+)
